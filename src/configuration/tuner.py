6726b503630c8e9d6a16a32f6c702ae879d351b6
ywhan2003 <2110520134@qq.com>
2024-09-26 14:51:16 +0800

    Add comments

--- src/configuration/tuner.py
+++ src/configuration/tuner.py
@@ -1,12 +1,24 @@
 from ..search.grid_search import GridSearch
 
 class Tuner:
+    ''' Tuner
+    
+    @attribute _file_name: The name of the targeted file
+    @attribute _need_compile: Whether the file need to be compiled
+    @attribute _params: All possible parameters
+    @attribute _search: The search algorithm
+    @attribute _best_params: The best parameters
+    '''
     def __init__(self, file_name: str, need_compile: bool, params: dict , search = 'grid_search') -> None:
+        ''' Initialize the tunner
+        
+        @param file_name: The name of the targeted file
+        @param need_compile: Whether the file need to be compiled
+        @param params: All possible parameters (eg: {block_size: (2, 4, 8), gcc_flag: (O1, O2, O3)})
+        '''
         self._file_name = file_name
         self._need_compile = need_compile
         self._params = params
-        print("tuner")
-        print(self._params)
         if search == 'grid_search':
             self._search = GridSearch(file_name, need_compile, params)
         self._best_params = {}
--- src/programs/input_program.py
+++ src/programs/input_program.py
@@ -1,18 +1,31 @@
 from ..configuration.tuner import Tuner
 
 class Program:
+    '''
+    Used to handle the input programs
+    
+    @attribute _file_name: The name of the targeted file
+    @attribute _need_compile: Whether the file need to be compiled
+    @attribute _params: All possible parameters
+    @attribute _best_params: The result of the tuner
+    @attribute _tuner: The tunner
+    '''
     def __init__(self, file_name: str, need_compile: bool, params: dict) -> None:
+        ''' Initialize the input programs
+        
+        @param file_name: The name of the targeted file
+        @param need_compile: Whether the file need to be compiled
+        @param params: All possible parameters (eg: {block_size: (2, 4, 8), gcc_flag: (O1, O2, O3)})
+        '''
         self._file_name = file_name
         self._need_compile = need_compile
         self._params = params
-        self._best_configuration = {}
-        print("program")
-        print(self._params)
+        self._best_params = {}
         self._tuner = Tuner(self._file_name, self._need_compile, self._params)
         
         
     def run(self):
-        self._best_configuration = self._tuner.get_best_params()
+        self._best_params = self._tuner.get_best_params()
     
     def get_best_params(self):
-        return self._best_configuration
+        return self._best_params
